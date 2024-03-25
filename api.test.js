79704adbf2d0ab166593d09d017342d150fd83d8
delcio <delciofrancisco1@gmail.com>
2024-03-25 11:52:41 +0100

    user case tests

--- api.test.js
+++ api.test.js
@@ -12,13 +12,46 @@ describe("Users Cases", () => {
     jest.clearAllMocks();
   });
 
-  // 
+  //Testing get users
   it("should fetch users", async () => {
     const users = [{ id: 1, name: "John" }];
     axios.get.mockResolvedValue({ data: { data: users } });
 
     const result = await fetchUsers();
     expect(result).toEqual(users);
+  });
+
+  //Testing add user case
+  it("should add a user", async () => {
+    const user = {
+      name: "Jane",
+      gender: "male",
+      email: "akinsiku18@gmail.com",
+      status: "active",
+    };
+    axios.post.mockResolvedValue({ data: { data: user } });
+
+    const result = await addUser(user);
+    expect(result).toEqual(user);
+  });
+
+  //Testing update user case
+  it("should update a user", async () => {
+    const id = 1;
+    const user = { name: "Jane" };
+    axios.put.mockResolvedValue({ data: { data: user } });
+
+    const result = await updateUser(id, user);
+    expect(result).toEqual(user);
+  });
+
+  //Testing delete user case
+  it("should delete a user", async () => {
+    const id = 1;
+    axios.delete.mockResolvedValue({ data: {} }); // No need to mock data for delete
+
+    const result = await deleteUser(id);
+    expect(result).toEqual({});
   });
 
   
--- index.js
+++ index.js
@@ -10,6 +10,41 @@ const fetchUsers = async () => {
     throw error;
   }
 };
+const addUser = async (data) => {
+  try {
+    const response = await axios.post(
+      "https://gorest.co.in/public/v2/users",
+      data
+    );
+    console.log({ response: response.data, context: "addUser" });
+    return response.data.data; // Extracting the 'data' property from the response
+  } catch (error) {
+    throw error;
+  }
+};
+
+const updateUser = async (id, data) => {
+  try {
+    const response = await axios.put(
+      `https://gorest.co.in/public/v2/users/${id}`,
+      data
+    );
+    return response.data.data; // Extracting the 'data' property from the response
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteUser = async (id) => {
+  try {
+    const response = await axios.delete(
+      `https://gorest.co.in/public/v2/users/${id}`
+    );
+    return response.data; // No need to extract 'data' property as it doesn't return deleted data
+  } catch (error) {
+    throw error;
+  }
+};
 
 
 module.exports = { fetchUsers };
